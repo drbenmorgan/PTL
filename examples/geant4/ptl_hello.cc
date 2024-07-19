563c98f0376e41f5650c7b4a06ae10cf59ff5fe3
Ben Morgan <ben.morgan@warwick.ac.uk>
2024-07-19 15:54:29 +0100

    Basic hello world example with ThreadPool

---
+++ examples/geant4/ptl_hello.cc
@@ -0,0 +1,50 @@
+#include "PTL/AutoLock.hh"
+#include "PTL/ThreadPool.hh"
+#include "PTL/Threading.hh"
+#include "PTL/Types.hh"
+
+#include <iostream>
+
+// Free Function
+void
+say_hello()
+{
+    // Purely so we don't mix outputs (comment out and see what happens...)!
+    PTL::AutoLock l(PTL::TypeMutex<decltype(std::cout)>());
+    std::cout << "Hello from thread " << PTL::GetThreadId() << std::endl;
+}
+
+// Simple Functor
+struct SaySomething
+{
+    void operator()() const
+    {
+        PTL::AutoLock l(PTL::TypeMutex<decltype(std::cout)>());
+        std::cout << "Functor running on " << PTL::GetThreadId() << std::endl;
+    }
+};
+
+int
+main()
+{
+    // Create a threadpool with default config (so threads = number of cores)
+    PTL::ThreadPool pool{ PTL::ThreadPool::Config{} };
+
+    // Can execute given function on each thread. Realistically only one returning
+    // void as otherwise return goes into the ether...
+    // NB: Is synchronous, so will only return after all executions are joined.
+    pool.execute_on_all_threads(say_hello);
+
+    // Can also pass functors
+    pool.execute_on_all_threads(SaySomething{});
+
+    // It also supports running lambdas
+    pool.execute_on_all_threads([]() {
+        PTL::AutoLock l(PTL::TypeMutex<decltype(std::cout)>());
+        std::cout << "Lamdba running on thread " << PTL::GetThreadId() << std::endl;
+    });
+
+    // At this low level in PTL, this function must be called before pool's destructor is
+    // called (Feels like a bug, or need for a higher level interface for pool creation)
+    pool.destroy_threadpool();
+}
